6b32e4f8f1344a2a02b2b0e363504b5aa5547c7a
Czebosak <czebosak@proton.me>
2024-12-16 20:36:11 +0100

    Added first solution and setup repo

---
+++ 01/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+    "os"
+    "bufio"
+    "fmt"
+    "strings"
+    "strconv"
+    "slices"
+)
+
+func read_input() ([]string, error) {
+    file, err := os.Open("input.txt")
+    if err != nil {
+        return nil, err
+    }
+
+    scanner := bufio.NewScanner(file)
+
+    var lines []string
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+
+    if err := scanner.Err(); err != nil {
+        return nil, fmt.Errorf("Could not read file: %w", err)
+    }
+
+    return lines, nil
+}
+
+func parse_input() ([]int, []int, error) {
+    lines, err := read_input()
+    if err != nil {
+        return nil, nil, err
+    }
+
+    var left_list []int
+    var right_list []int
+    for _, line := range lines {
+        data := strings.Split(line, " ")
+
+        left_num, err := strconv.Atoi(data[0])
+        if err != nil { return nil, nil, err }
+        right_num, err := strconv.Atoi(data[3])
+        if err != nil { return nil, nil, err }
+
+        left_list = append(left_list, left_num)
+        right_list = append(right_list, right_num)
+    }
+
+    return left_list, right_list, nil
+}
+
+func main() {
+    left_list, right_list, err := parse_input()
+    if err != nil {
+        panic(err)
+    }
+
+    slices.Sort(left_list)
+    slices.Sort(right_list)
+
+    total_distance := 0
+    
+    for i := 0; i < len(left_list); i++ {
+        distance := left_list[i] - right_list[i]
+        if distance < 0 { distance = -distance }
+
+        total_distance += distance
+    }
+
+    fmt.Printf("%d\n", total_distance)
+}
+
